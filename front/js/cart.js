32aaa76ff85c4f4d72b93972cf694348f8d42efd
antirandominette <choletl@protonmail.com>
2022-11-15 10:44:14 +0100

    reworked functions to get sensible data && factorized cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,13 @@
 let cartProducts;
 
+const allProducts = [];
+const numberFormat = new Intl.NumberFormat('fr-FR');
 const cartItems = document.querySelector('#cart__items');
 const cartTotalQuantity = document.querySelector('#totalQuantity');
 const cartTotalPrice = document.querySelector('#totalPrice');
 
 function getProductsFromLocalStorage() {
     cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
-
     console.log(cartProducts);
 }
 
@@ -16,46 +17,57 @@ function updateLocalStorage() {
     if(cartProducts.length === 0 ? localStorage.removeItem('cartProducts') : null);
 }
 
+async function getAllProducts() {
+    await fetch('http://localhost:3000/api/products')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(product => {
+            allProducts.push(product);
+        });
+    });
+
+    console.log(allProducts);
+    mapThroughAllProducts();
+}
+
 function mapThroughAllProducts() {
-    if(cartProducts ?
-        cartProducts.forEach((product, i) => {
-            fetch(`http://localhost:3000/api/products/${product.id}`)
-            .then(res => res.json())
-            .then(data => {
-                let price = data.price;
-                let numberFormat = new Intl.NumberFormat('fr-FR');
-                let formattedPrice = numberFormat.format(price);
+    if(cartProducts ? 
+        cartProducts.forEach(product => {
+            const item = allProducts.find(x => x._id === product.id);
+            
+            let price = item.price;
+            let formattedPrice = numberFormat.format(price);
 
-                cartItems.innerHTML += `
-                    <article class="cart__item" data-id="${product.id}" data-color="${product.selectedColor}">
-                        <div class="cart__item__img">
-                            <img src="${data.imageUrl}" alt="${data.altTxt}">
+            cartItems.innerHTML += `
+                <article class="cart__item" data-id="${item._id}" data-color="${product.selectedColor}">
+                    <div class="cart__item__img">
+                        <img src="${item.imageUrl}" alt="${item.altTxt}">
+                    </div>
+                    <div class="cart__item__content">
+                        <div class="cart__item__content__description">
+                            <h2>${item.name}</h2>
+                            <p>${product.selectedColor}</p>
+                            <p>${formattedPrice} €</p>
                         </div>
-                        <div class="cart__item__content">
-                            <div class="cart__item__content__description">
-                                <h2>${data.name}</h2>
-                                <p>${product.selectedColor}</p>
-                                <p>${formattedPrice} €</p>
+                        <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                                <p>Qté : </p>
+                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
                             </div>
-                            <div class="cart__item__content__settings">
-                                <div class="cart__item__content__settings__quantity">
-                                    <p>Qté : </p>
-                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                                </div>
-                                <div class="cart__item__content__settings__delete">
-                                    <p class="deleteItem">Supprimer</p>
-                                </div>
+                            <div class="cart__item__content__settings__delete">
+                                <p class="deleteItem">Supprimer</p>
                             </div>
                         </div>
-                    </article>
-                `;
+                    </div>
+                </article>
+            `;
 
-                calculateTotalPrice(price);
-                calculateTotalQuantity();
-                listenToDeleteButtons(price);
-                listenToItemQuantityInputs(price);
-            });
-        }) : displayEmptyCartMsg());
+            calculateTotalPrice(price);
+            calculateTotalQuantity();
+            listenToDeleteButtons(price);
+            listenToItemQuantityInputs(price);
+        })    
+    : displayEmptyCartMsg()); 
 }
 
 function calculateTotalQuantity() {
@@ -65,12 +77,11 @@ function calculateTotalQuantity() {
         totalQuantity += parseInt(product.quantity);
     });
 
-    cartTotalQuantity.innerHTML = totalQuantity;
+    cartTotalQuantity.innerHTML = `${numberFormat.format(totalQuantity)}`;
 }
 
 function calculateTotalPrice(price) {
     let totalPrice = 0;
-    let numberFormat = new Intl.NumberFormat('fr-FR');
 
     cartProducts.forEach(product => {
         totalPrice += parseInt(price) * parseInt(product.quantity);
@@ -91,7 +102,7 @@ function listenToDeleteButtons(price) {
             const productColor = article.dataset.color;
 
             // determine the product to delete via its id and color and store it in a variable
-            const productToDelete = cartProducts.findIndex(e => e.id === productId && e.selectedColor === productColor);
+            const productToDelete = cartProducts.findIndex(x => x.id === productId && x.selectedColor === productColor);
             
             cartProducts.splice(productToDelete, 1);
             article.remove();
@@ -101,7 +112,10 @@ function listenToDeleteButtons(price) {
             calculateTotalQuantity();
             calculateTotalPrice(price);
 
-            if(!localStorage.getItem('cartProducts') ? displayEmptyCartMsg() : console.log(`Il y a encore des produits dans le panier ${ cartProducts.length }`));
+            if(!localStorage.getItem('cartProducts') ? 
+                displayEmptyCartMsg() 
+                : console.log(`Il y a encore des produits dans le panier ${ cartProducts.length }`
+            ));
         });
     });
 }
@@ -129,18 +143,6 @@ function displayEmptyCartMsg() {
 }
 
 function listenToOrderButton() {
-    const orderButton = document.queryselector("order");
-
-    orderButton.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        if(cartProducts) {
-            fetch(`http://localhost:3000/api/products/order${id}`, {})
-        }
-    })
-}
-
-function sendOrder() {
     orderButton = document.querySelector("#order");
     
     orderButton.addEventListener('click', (e) => {
@@ -156,28 +158,31 @@ function sendOrder() {
 
         const products = [];
 
-        cartProducts.forEach(product => {
-            products.push(product.id);
-        });
-
-        console.log(products);
-
-        fetch('http://localhost:3000/api/products/order', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ contact, products })
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            window.location.replace(`confirmation.html?orderId=${data.orderId}`)
-        })
+        if(cartProducts) {
+            cartProducts.forEach(product => {
+                products.push(product.id);
+            });
+    
+            console.log(products);
+    
+            fetch('http://localhost:3000/api/products/order', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ contact, products })
+            })
+            .then(data => {
+                console.log(data);
+                window.location.replace(`confirmation.html?orderId=${data.orderId}`)
+            })
+        }
+        else {
+            alert('Votre panier est vide');
+        }
     });
 }
 
-
 getProductsFromLocalStorage();
-mapThroughAllProducts();
-sendOrder(); 
+getAllProducts();
+listenToOrderButton(); 
